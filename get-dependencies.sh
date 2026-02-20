33d33f6b805fafa09c3f7f3d6840f63ccc4bd752
Link4Electronics <link4electronics@gmail.com>
2026-02-20 17:29:03 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,7 +19,7 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package 
 
 # If the application needs to be manually built that has to be done down here
-echo "Making nightly build of Open Horizon..."
+echo "Making nightly build of Ymir..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/StrikerX3/Ymir"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
